5d89442fcd1b4b6223fe50cdd4fc002b72291856
zaffarani.kaique@gmail.com <zaffarani.kaique@gmail.com>
2024-04-20 00:48:31 -0300

    registradora

--- Aula 19-04/registradora.py
+++ Aula 19-04/registradora.py
@@ -7,10 +7,29 @@ Código: | Preço:
   9     | 8,00
 '''
 
-        
+print(msg)
+c = 2
 
-
-
-
-
+while c != 0:
+  a = int(input("Código: "))
+  b = int(input("Quantidade: "))
+  c = int(input("Adicionar (1), parar (0): "))
+  d = 0
+  e = a + d
+    
+  if a == 1:
+    d = b * 0.5
+  elif a == 2:
+    d = b * 1
+  elif a == 3:
+    d = b * 4
+  elif a == 5:
+    d = b * 7
+  elif a == 9:
+    d = b * 8
+  else:
+    int(input("Código inválido."))
+    break
+    
+print(f"Você pagará por R${e}")
 
