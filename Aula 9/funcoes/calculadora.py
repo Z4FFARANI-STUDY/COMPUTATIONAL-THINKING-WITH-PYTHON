5f774740ab2f1b71e7708a4c70d30aa52b44a7d4
Z4ffarani <zaffarani.kaique@gmail.com>
2024-05-11 00:31:39 -0300

    fixing calculator

--- Aula 9/funcoes/calculadora.py
+++ Aula 9/funcoes/calculadora.py
@@ -1,41 +1,34 @@
-continuar = "S"
+continuar = "C"
 
-while continuar == "S":
-    operacao = input("Escolha a operação (+) (-) (/) (*): ")
+while continuar == "C":
     n1 = int(input("Número 1: "))
     n2 = int(input("Número 2: "))
-
-    def somar(n1, n2):
-        return n1 + n2
-
-    def subtrair(n1, n2):
-        return n1 - n2
-
-    def dividir(n1, n2):
-        return n1 / n2
-
-    def multiplicar(n1, n2):
-        return n1 * n2
-
-    if operacao == "+":
-        print("Soma: ")
-        print(somar(n1, n2))
-
-    elif operacao == "-":
-        print("Subtração: ")
-        print(subtrair(n1, n2))
-
-    elif operacao == "/":
-        print("Divisão: ")
-        print(dividir(n1, n2))
-
-    elif operacao == "*":
-        print("Multiplicar: ")
-        print(multiplicar(n1, n2))
+    pergunta = input("Escolha a operação (+) (-) (/) (*): ")
+ 
+    def operacao(n1, n2):
+        if pergunta == "+":
+            return n1 + n2
+        elif pergunta == "-":
+            return n1 - n2
+        elif pergunta == "/":
+            return n1 / n2
+        elif pergunta == "*":
+            return n1 * n2
+        else:
+            print("Operação inválida, tente novamente.")
     
+    if operacao(n1, n2) == None:
+        continuar = str(input("Continuar <C>, Parar <P>: ")).upper()
     else:
-        print("Operação inválida, tente novamente.")
+        print(f"Resposta: {operacao(n1, n2)}")
+        continuar = str(input("Continuar <C>, Parar <P>: ")).upper()    
 
-    continuar = input("Continuar? SIM <S>, NÃO <N>: ").upper()
+    
+
+        
+    
+
+    
+
 
 
