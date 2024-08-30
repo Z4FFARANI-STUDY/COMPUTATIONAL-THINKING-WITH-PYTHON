0b6ccd9c0e99629b9bc5a5bf890af9588ce8ecd0
Z4ffarani <zaffarani.kaique@gmail.com>
2024-08-30 15:32:07 -0300

    comentário

--- Semestre 2/Aula 14/cria_matriz.py
+++ Semestre 2/Aula 14/cria_matriz.py
@@ -3,9 +3,9 @@ l = 2
 
 def cria_matriz(c, l):
     matriz = []
-    for j in range(l): #Passa pela quantidade de linhas da variável "l"
+    for j in range(l): #Passa pela quantidade de linhas da variável "l", que no caso são linhas de uma matriz
         linha = []
-        for i in range(c): #Passa pela quantidade de colunas da variável "c"
+        for i in range(c): #Passa pela quantidade de colunas da variável "c", que no caso são colunas de uma matriz
             linha.append(int(input(":")))
         matriz.append(linha)
     return matriz
