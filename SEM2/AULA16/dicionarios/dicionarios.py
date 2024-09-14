3b384c730b6d7098234aedc09a6626362dab430f
Z4ffarani <zaffarani.kaique@gmail.com>
2024-09-14 02:39:24 -0300

    update

--- SEM2/AULA16/dicionarios/dicionarios.py
+++ SEM2/AULA16/dicionarios/dicionarios.py
@@ -1,22 +1,26 @@
-# dict = {
-#     'Carro': 'BMW',
-#     'Propriedade': {'Motor': '300cv',
-#                     'Valor': 100000,
-#                     'Ar': True
-#                     }
-# }
-# dict['Carro'] = 'Ferrari'
-# print(dict.get('Modelo'))
-# print('Modelo' in dict)
-
-
-dictA = {
+dict = {
     'Carro': 'BMW',
-    'Propriedade': 'Porta',
-    'Ar': False,
-    (1,0,1): (3.5)
+    'Propriedade': {'Motor': '300cv',
+                    'Valor': 100000,
+                    'Ar': True
+                    }
 }
+# Troca de elemento a partir da chave 'Carro'
+dict['Carro'] = 'Ferrari'
 
+# Busca por uma chave
+print(dict.get('Modelo'))
+print('Modelo' in dict)
+
+
+# dictA = {
+#     'Carro': 'BMW',
+#     'Propriedade': 'Porta',
+#     'Ar': False,
+#     (1,0,1): (3.5)
+# }
+
+# # remove a chave 'Carro' do dicionário
 # print(dictA.pop('Carro'))
 # # print(list(dictA.items()))
 # dictA['Carro'] = 'Ferrari'
@@ -29,11 +33,11 @@ dictA = {
 #     print(f'Chave {i}; Valor {j}')
 
 
-dictB = {
-    'Propriedade': 'Janela',
-    'Ar': True
-}
+# dictB = {
+#     'Propriedade': 'Janela',
+#     'Ar': True
+# }
 
-# Subscreve os elementos / adiciona em outro dicionário
-dictA.update(dictB)
-print(dictA)
+# # Subscreve os elementos / adiciona em outro dicionário
+# dictA.update(dictB)
+# print(dictA)
