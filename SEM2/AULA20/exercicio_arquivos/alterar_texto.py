85edbd3dc46851ca73d6c62f86578e1c8b75c9d8
Z4ffarani <zaffarani.kaique@gmail.com>
2024-10-19 01:18:57 -0300

    few update

--- SEM2/AULA20/exercicio_arquivos/alterar_texto.py
+++ SEM2/AULA20/exercicio_arquivos/alterar_texto.py
@@ -1,3 +1,9 @@
-arquivo = open('COMPUTATIONAL-THINKING-WITH-PYTHON/SEM2/AULA20/exercicio_arquivos/alterar_texto.txt', 'w', encoding='UTF-8')
+arquivo = open('COMPUTATIONAL-THINKING-WITH-PYTHON/SEM2/AULA20/exercicio_arquivos/alterar_texto.txt', 'r+', encoding='UTF-8')
 
-arquivo.write('A')
+for i in arquivo:
+    # não esquecer de reposicionar até o início do arquivo
+    arquivo.seek(0)
+    for j in i:
+        if j == 'a':
+            j = 'A'
+        arquivo.write(j)
